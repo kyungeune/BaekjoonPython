082bf1ae4eff0bac771b3753ec3865dcb70c0361
kyungeune <132311633+kyungeune@users.noreply.github.com>
2025-05-16 19:48:37 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 12.79 ms, Memory: 9.41 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,35 +1,31 @@
 from collections import deque
-def solution(maps):
-    n=len(maps)
-    m=len(maps[0])
-    visited=[[False for i in range(len(maps[0]))] for j in range(len(maps))]
-    
-    
-    dx = [-1,1,0,0]
-    dy = [0,0,1,-1]
 
-    def BFS(x,y):
-        q=deque()
+def solution(maps):
+    n = len(maps)
+    m = len(maps[0])
+    
+    visited=[[False for i in range(m)] for j in range(n)]
+    
+    dx=[1,-1,0,0]
+    dy=[0,0,1,-1]
+    
+    def BFS(x, y):
+        q = deque()
         q.append((x,y))
-        visited[x][y]=True
+        visited[x][y]=1
         
         while q:
             x,y = q.popleft()
-            
             for i in range(4):
                 nx = x+dx[i]
                 ny = y+dy[i]
-
-                if nx<0 or nx>=n or ny<0 or ny>=m:
-                    continue
-                if maps[nx][ny] == 1 and not visited[nx][ny]:
-                    visited[nx][ny]=True
-                    maps[nx][ny] = maps[x][y] + 1
-                    q.append((nx,ny))
-
-        if visited[n-1][m-1]==False:
-            return -1
-        else:
-            return maps[n-1][m-1]
-        
-    return BFS(0,0)
+                
+                if 0<=nx<n and 0<=ny<m:
+                    if visited[nx][ny]==False and maps[nx][ny]==1:
+                        visited[nx][ny] = visited[x][y] + 1
+                        q.append((nx,ny))
+    
+    BFS(0,0)
+    if visited[n-1][m-1] == False:
+        return -1
+    return visited[n-1][m-1]
